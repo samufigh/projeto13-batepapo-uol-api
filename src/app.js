326c2d26c1f309bb0a13bb09967712895cb703b4
samufigh <samufigh21@gmail.com>
2023-07-01 19:48:04 -0300

    Requisitos: Armazenamento dos dados (mongodb)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
 
-app.get("/", (req, res) => {
-    res.send("FUNCIONOOU");
+
+
+mongoClient.connect()
+    .then( () => db = mongoClient.db())
+    .catch( (err) => console.log(err.message));
+
+app.post("/", async(req, res) => {
+    try {
+        
+
+        res.send("FUNCIONOOU");
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
 })
 
 app.listen(5000, ()=> console.log("Rodando na porta 5000"));
